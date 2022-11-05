575f451a2a090c06da23a49f46c915727659e1d3
deejeexo <gotenks257@gmail.com>
2022-11-05 19:00:29 +0200

    resolved comments

--- simpleclass.java
+++ simpleclass.java
@@ -1,24 +1,69 @@
-public static void main(String[] args) {
-   caughtspeedOfTheCaring (60,false);
-   caughtspeedOfTheCaring (65,false);
-   caughtspeedOfTheCaring (65,true);
+public static class TicketCalculator {
+    protected int NO_TICKET_MAX_SPEED = 60;
+    protected int SMALL_TICKET_MIN_SPEED = 61;
+    protected int SMALL_TICKET_MAX_SPEED = 80;
+    protected int BIG_TICKET_MIN_SPEED = 81;
+
+    public int getTicket(int speed) {
+    if (isNoTicket(speed)) {
+      return Ticket.NO_TICKET;
+    } else if (isSmallTicket(speed)) {
+      return Ticket.SMALL_TICKET;
+    } else if (isBigTicket(speed)) {
+      return Ticket.BIG_TICKET;
+    } else {
+      return Ticket.ERROR_CODE;
+    }
+}
+
+    private boolean isNoTicket(int speed) {
+    return speed <= NO_TICKET_MAX_SPEED; 
+  }
+
+    private boolean isSmallTicket(int speed) {
+    return speed >= SMALL_TICKET_MIN_SPEED && speed <= SMALL_TICKET_MAX_SPEED;
+  }
+
+    private boolean isBigTicket(int speed) {
+    return speed >= BIG_TICKET_MIN_SPEED;
+  }
  }
- public static void caughtspeedOfTheCaringOfTheCar (int speedOfTheCar, boolean isBirthday) {
- if (isBirthday) {
-     if(speedOfTheCar<=45) {       
-      System.out.println(0);     
-     }else if (speedOfTheCar>=75 && speedOfTheCar<=85) {       
-      System.out.println(1);
-     }else if (speedOfTheCar>=86) {
-       System.out.println(2);
-     }
-   }else {
-     if(speedOfTheCar<=60) {       
-       System.out.println(0);     
-    }else if (speedOfTheCar>=61 && speedOfTheCar <=88) {       
-      System.out.println(1);     
-    }else if(speedOfTheCar>=81) {
-       System.out.println(2);
-     }
+
+ public static class TicketCalculatorOnMotorway extends TicketCalculator {
+  public TicketCalculatorOnMotorway() {
+    super();
+    adjustSpeedLimitsForMotorway();
+  }
+
+  public static void generateTicket (int speed, boolean isMotorway) {
+    TicketCalculator ticket;
+    if (!isMotorway) {
+    ticket = new TicketCalculator();
+  } else {
+    ticket = new TicketCalculatorOnMotorway();
+  }
+    System.out.println(ticket.getTicket(speed));
+ }
+
+  private void adjustSpeedLimitsForMotorway() {
+    int adjustSpeedBy = 10;
+
+    NO_TICKET_MAX_SPEED += adjustSpeedBy;
+    SMALL_TICKET_MIN_SPEED += adjustSpeedBy;
+    SMALL_TICKET_MAX_SPEED += adjustSpeedBy;
+    BIG_TICKET_MIN_SPEED += adjustSpeedBy;
    }
+}
+
+ public class Ticket {
+  public final static string NO_TICKET = "No ticket";
+  public final static string SMALL_TICKET = "Small ticket";
+  public final static string BIG_TICKET = "Big ticket";
+  public final static string ERROR_CODE = "Error";
+}
+
+ public static void main(String[] args) {
+    generateTicket(60, false);
+    generateTicket(65, false);
+    generateTicket(65, true);
  }
