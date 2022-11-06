f5304cd5098f8b52bf7ec9e89d13b518885f6320
Elvinas Skukauskas <elvinas.skukauskas@stud.vilniustech.lt>
2022-11-06 16:37:44 +0200

    Resolved comments and improved readability

--- src/LoanCalculator.java
+++ src/LoanCalculator.java
@@ -1,70 +1,93 @@
 public class LoanCalculator {
-    public static class Loan {
-        public double lA;
-        public int t;
-        public double r;
-        public double p;
+    final static double MIN_LOAN_AMOUNT = 100;
+    final static double MIN_LOAN_TERM = 1;
+    final static double MAX_LOAN_TERM = 50;
+    final static double MIN_INTEREST_RATE = 0;
+    final static double MIN_DOWN_PAYMENT = 0;
 
-        public Loan (double lA, int t, double r, double p) {
-            this.lA = lA;
-            this.t = t;
-            this.r = r;
-            this.p = p;
+    public static class Loan {
+        public double loanAmount;
+        public int term;
+        public double rate;
+        public double downPayment;
+
+        public Loan (double loanAmount, int term, double rate, double downPayment) {
+            this.loanAmount = loanAmount;
+            this.term = term;
+            this.rate = rate;
+            this.downPayment = downPayment;
         }
     }
 
     public static void main(String[] args) {
         Loan loan = new Loan(500, 2, 2.1, 100);
-        LoanCalc(loan);
+        validateLoan(loan);
+        loanCalc(loan);
     }
 
-    public static void LoanCalc(Loan loan){
-        do {
-            if(loan.lA <= 99){
-                System.out.println("Loan amount must be greater than $99\n");
-            }
-        } while(loan.lA <= 99);
+    private static void loanCalc(Loan loan){
+        int termsInMonths = loan.term * 12;
+        loan.loanAmount -= loan.downPayment;
 
-        do {
-            if(loan.t <= 0){
-                System.out.println("Loan term must be at least one year\n");
-            }
-            else if(loan.t > 50){
-                System.out.println("Loan term cannot exceed 50 years\n");
-            }
-        } while(loan.t <= 0 || loan.t > 50);
+        double monthlyPayment = calculateMonthlyPayment(loan, termsInMonths);
+        double interest = calculateInterest(loan, termsInMonths);
 
-        do {
-            if(loan.r < 0){
-                System.out.println("Interest rate cannot be negative\n");
-            }
-        } while(loan.r < 0);
+        printCalculationResults(monthlyPayment, interest);
+    }
 
-        do {
-            if(loan.p < 0){
-                System.out.println("Down payment cannot be negative, but can be $0\n");
-            }
-            else if(loan.p >= loan.lA){
-                System.out.println("Down payment cannot equal or exceed your loan amount\n");
-            }
-        } while(loan.p < 0 || loan.p >= loan.lA);
-        System.out.println();
+    private static double calculateInterest(Loan loan, int termsInMonths) {
+        double totalCost = loan.downPayment * termsInMonths;
+        return totalCost - loan.loanAmount;
+    }
 
-        int terms = loan.t * 12;
-        loan.lA -= loan.p;
-        //Calculate rate
-        double rate = (loan.r / 100.0) / 12;
-        double payment = (rate * loan.lA) / (1 - Math.pow((1 + rate), -terms));
+    private static double calculateMonthlyPayment(Loan loan, int termsInMonths) {
+        double monthlyRate = (loan.rate / 100.0) / 12;
+        return (monthlyRate * loan.loanAmount) / (1 - Math.pow((1 + monthlyRate), -termsInMonths));
+    }
 
-        //Calculate interest
-        double totalCost = payment * terms;
-        double interest = totalCost - loan.lA;
-
+    private static void printCalculationResults(double payment, double interest) {
         System.out.println("Monthly Payments: ");
         System.out.print("$");
         System.out.printf("%.2f", payment);
         System.out.println();
         System.out.println("Total Accrued Interest: ");
         System.out.println("$" + (int)Math.round(interest));
+    }
+
+    private static void validateLoan(Loan loan){
+        String validationErrors = "";
+        if(loan.loanAmount < MIN_LOAN_AMOUNT){
+            validationErrors += "Loan amount must be greater than $99;\n";
+        }
+
+        if(loan.term < MIN_LOAN_TERM){
+            validationErrors += "Loan term must be at least one year;\n";
+        }
+        else if(loan.term > MAX_LOAN_TERM){
+            validationErrors += "Loan term cannot exceed 50 years;\n";
+        }
+
+        if(loan.term < MIN_LOAN_TERM){
+            validationErrors += "Loan term must be at least one year;\n";
+        }
+        else if(loan.term > MAX_LOAN_TERM){
+            validationErrors += "Loan term cannot exceed 50 years;\n";
+        }
+
+        if(loan.rate < MIN_INTEREST_RATE){
+            validationErrors += "Interest rate cannot be negative;\n";
+        }
+
+        if(loan.downPayment < MIN_DOWN_PAYMENT){
+            validationErrors += "Down payment cannot be negative, but can be $0;\n";
+        }
+        else if(loan.downPayment >= loan.loanAmount){
+            validationErrors += "Down payment cannot equal or exceed your loan amount;\n";
+        }
+
+        if (!validationErrors.isEmpty()){
+            System.out.println(validationErrors);
+            System.exit(0);
+        }
     }
 }
